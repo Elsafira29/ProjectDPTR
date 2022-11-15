a734a95936a89c9630030049185c5e51d87db5a2
R4BIATUL352 <elsafira.dewantari@students.amikom.ac.id>
2022-11-15 14:15:35 +0700

    tambah view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,7 +30,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active"><a href="#">Log Out</a></li>
+              <li class="breadcrumb-item active"><a href="{{ route('actionlogout') }}">Log Out</a></li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
