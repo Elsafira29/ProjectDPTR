6d69c5ea9de793efd98d974cbbd928816aab43a5
R4BIATUL352 <elsafira.dewantari@students.amikom.ac.id>
2022-11-15 12:42:12 +0700

    Ngubah title

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aplikasi Pengarsipan DPTR</title>
+    <title>Pengarsipan Pertanahan DPTR</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
